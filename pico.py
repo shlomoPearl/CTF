3e337e62e83ee718c232df0314109d1cdccfca2a
shlomoPearl <shlomoper94@gmail.com>
2025-11-02 13:49:23 +0200

    read pdf meta data and decode base64

--- pico.py
+++ pico.py
@@ -1,5 +1,6 @@
 import requests
-
+import base64
+from pypdf import PdfReader
 # crack the gate:
 PORT=6666 # insert the right port here 
 url = f'http://amiable-citadel.picoctf.net:{PORT}'
@@ -8,3 +9,11 @@ payload={'email':'ctf-player@picoctf.org', 'password':'PASSWORD'}
 requests.post(url,json=payload,headers=h)
 print(r.status_code)
 print(r.text)
+
+# read pdf metadata and decode it from base64
+r = PdfReader('path_t_pdf')
+meta = r.metadata
+print(meta)
+d = meta.author
+print(base64.b64decode(d))
+
